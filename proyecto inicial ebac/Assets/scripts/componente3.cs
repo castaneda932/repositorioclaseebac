479d1c3883c9e812f2c023432e3a2e210da37693
castaneda932 <castachatz90@outlook.com>
2023-07-28 01:34:03 -0600

    añadimos scripts
    -se añadieron nuevos scripts

---
+++ proyecto inicial ebac/Assets/scripts/componente3.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class componente3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        componente1.miObjeto.name = "nuevoNombreDeMiObjeto";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
