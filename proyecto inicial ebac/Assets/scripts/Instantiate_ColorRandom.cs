c8aecd6bd64bdc8b5ca0fa1ecfb2790538171085
castaneda932 <castachatz90@outlook.com>
2023-08-16 10:33:00 -0600

    se añadio instantiate
    -se añadio un instantiate al crear cubo

--- proyecto inicial ebac/Assets/scripts/Instantiate_ColorRandom.cs
+++ proyecto inicial ebac/Assets/scripts/Instantiate_ColorRandom.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class Instantiate_ColorRandom : MonoBehaviour
 {
+    public GameObject prefabcube;
+    public List<GameObject> listadecubos;
+    public int numcubos = 0;
+
+       
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        listadecubos = new List<GameObject>();
+        GameObject gameObject = Instantiate(prefabcube);
         
+       
+
     }
 
     // Update is called once per frame
