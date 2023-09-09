abf9edef4ac189e72fb3b5f48185a1e8f9f92621
castaneda932 <castachatz90@outlook.com>
2023-09-09 16:16:58 -0600

    se crearon variables de primeros 2 GO
    -se crearon las variables de los primeros 2 GO para que uno me diera falso y otro verdadero

--- proyecto inicial ebac/Assets/scripts/Bool2.cs
+++ proyecto inicial ebac/Assets/scripts/Bool2.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Bool2 : MonoBehaviour
 {
+
+    bool variable1;
+    bool variable2;
+    bool variable3;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,24 @@ public class Bool2 : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        variable1 = true;
+        variable2 = false;
+        if (variable1 || variable2)
+        {
+            Debug.Log("la variable es verdadera");
+            Color c = Color.white;
+            gameObject.GetComponent<MeshRenderer>().material.color = c;
+        }
+        else
+        {
+            Debug.Log("la variable es falsa");
+            Color c = Color.black;
+            gameObject.GetComponent<MeshRenderer>().material.color = c;
+        }
+
     }
 }
