605dd92723ea55f61685a75f8c0593108cf03964
castaneda932 <castachatz90@outlook.com>
2023-09-13 09:58:13 -0600

    actualizacion de variables
    -se corrigieron las variables en todos los GO

--- proyecto inicial ebac/Assets/scripts/Bool5.cs
+++ proyecto inicial ebac/Assets/scripts/Bool5.cs
@@ -5,34 +5,26 @@ using UnityEngine;
 public class Bool5 : MonoBehaviour
 {
 
-    Bool3 bool3;
-    Bool4 bool4;
+    public Bool3 bool3;
+    public Bool4 bool4;
+    public bool variable5;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void FixedUpdate()
     {
-        if (bool3 && bool4)
+        if (bool3.variable3 && bool4.variable4)
         {
             Debug.Log("la variable es verdadera");
             Color c = Color.white;
             gameObject.GetComponent<MeshRenderer>().material.color = c;
+            variable5 = true;
         }
         else
         {
             Debug.Log("la variable es falsa");
             Color c = Color.black;
             gameObject.GetComponent<MeshRenderer>().material.color = c;
+            variable5 = false;
         }
 
 
