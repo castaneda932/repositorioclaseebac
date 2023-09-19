ccb1a0103c4cfc796c093f5b9e74c23653e944c1
castaneda932 <castachatz90@outlook.com>
2023-09-18 23:17:27 -0600

    se crearon 2 variables y se utilizo el operador modulo
    -se crearon dos variables float y se hizo una operación entre ellas para dar numero entero por medio de un casteo explicito.
    -se utilizo el operador modulo para cambiar el color del gameobject si es el numero es par o impar.

--- proyecto inicial ebac/Assets/scripts/EjerciciosVariablesMod8.cs
+++ proyecto inicial ebac/Assets/scripts/EjerciciosVariablesMod8.cs
@@ -7,6 +7,26 @@ public class EjerciciosVariablesMod8 : MonoBehaviour
     public int miVariable = 1;
     public float miVariable1 = 1.0f;
 
+    public GameObject GameObject;
+
+
+   
+
+
+
+    private void Start()
+    {
+        float variable1 = 10.5f;
+        float variable2 = 3.75f;
+
+        float resultadoFloat = variable1 / variable2;
+        int resultadoEntero = (int)resultadoFloat;
+
+        Debug.Log("El resultado en float es:" + resultadoFloat);
+        Debug.Log("El resultado en entero (con casting explicito) es:" + resultadoEntero);
+
+
+    }
     // Update is called once per frame
     void Update()
     {
@@ -18,6 +38,18 @@ public class EjerciciosVariablesMod8 : MonoBehaviour
     {
         miVariable1 *= 1.5f;
         Debug.Log("El valor actual de la variable es: " + miVariable1);
+
+        int numero = 8;
+        if (numero % 2 == 0)
+        {
+            Debug.Log(numero + "es un numero par.");
+            gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        else 
+        {
+            Debug.Log(numero + "es un numero impar.");
+            gameObject.GetComponent<Renderer>().material.color = Color.black;
+        }
 
     }
 }
