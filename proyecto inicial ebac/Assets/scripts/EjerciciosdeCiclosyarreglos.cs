7a922ca50ad4c46fcccfc6a212a2ad7cfb2fdefa
castaneda932 <castachatz90@outlook.com>
2023-09-24 01:10:11 -0600

    Se hizo la multiplicación de matriz
    -parte 3 te la actividad
    -se hizo la multiplicación de matriz

--- proyecto inicial ebac/Assets/scripts/EjerciciosdeCiclosyarreglos.cs
+++ proyecto inicial ebac/Assets/scripts/EjerciciosdeCiclosyarreglos.cs
@@ -7,6 +7,7 @@ public class EjerciciosdeCiclosyarreglos : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //parte1
         {
             int[] arr1 = new int[10]; 
             int[] arr2 = new int[10]; 
@@ -31,6 +32,9 @@ public class EjerciciosdeCiclosyarreglos : MonoBehaviour
             Debug.Log("arr3: " + string.Join(", ", arr3));
         }
 
+
+        //parte2
+
         string[] palabras = new string[]
         {
             "Esto", "es", "muy", "complicado", "de", "hacer"
@@ -38,14 +42,54 @@ public class EjerciciosdeCiclosyarreglos : MonoBehaviour
 
         string oracionCompleta = "";
 
-        // Usando un ciclo foreach para concatenar las palabras
         foreach (string palabra in palabras)
         {
             oracionCompleta += palabra + " ";
         }
-
-        // Imprimir la oración completa en la consola
         Debug.Log(oracionCompleta);
+
+
+
+
+        //parte3
+
+        int[,] arregloBidimensional = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+
+         
+        int[] arregloUnidimensional = new int[2] { 7, 8 };
+
+        
+      
+        
+        int[,] resultado = MultiplicarMatrices(arregloBidimensional, arregloUnidimensional);
+
+        
+        Debug.Log("Resultado de la multiplicación:");
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Debug.Log(resultado[i, j]);
+            }
+        }
+    }
+
+    int[,] MultiplicarMatrices(int[,] matrizA, int[] matrizB)
+    {
+        int filasA = matrizA.GetLength(0);
+        int columnasA = matrizA.GetLength(1);
+
+        int[,] resultado = new int[filasA, columnasA];
+
+        for (int i = 0; i < filasA; i++)
+        {
+            for (int j = 0; j < columnasA; j++)
+            {
+                resultado[i, j] = matrizA[i, j] * matrizB[i];
+            }
+        }
+
+        return resultado;
 
 
 
