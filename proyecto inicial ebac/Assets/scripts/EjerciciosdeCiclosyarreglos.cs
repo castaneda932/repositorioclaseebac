2d3b77432675ca2f91683744df3326bb6f498183
castaneda932 <castachatz90@outlook.com>
2023-09-24 00:33:09 -0600

    Arreglos bidimensionales
    -primera parte de la actividad
    -se hicieron los arreglos bidimensionales

--- proyecto inicial ebac/Assets/scripts/EjerciciosdeCiclosyarreglos.cs
+++ proyecto inicial ebac/Assets/scripts/EjerciciosdeCiclosyarreglos.cs
@@ -7,12 +7,28 @@ public class EjerciciosdeCiclosyarreglos : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        {
+            int[] arr1 = new int[10]; 
+            int[] arr2 = new int[10]; 
+            int[] arr3 = new int[10]; 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            
+            for (int i = 0; i < 10; i++)
+            {
+                arr1[i] = Random.Range(1, 25); 
+                arr2[i] = Random.Range(1, 25);
+            }
+
+            
+            for (int i = 0; i < 10; i++)
+            {
+                arr3[i] = arr1[i] + arr2[i];
+            }
+
+            
+            Debug.Log("arr1: " + string.Join(", ", arr1));
+            Debug.Log("arr2: " + string.Join(", ", arr2));
+            Debug.Log("arr3: " + string.Join(", ", arr3));
+        }
     }
 }
